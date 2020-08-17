05fd40b140647847cc01d18137c8ebe94a563faf
bearkid1785 <69457316+bearkid1785@users.noreply.github.com>
2020-08-17 14:37:12 +0900

    Add files via upload

---
+++ JoinCat/src/JoinCat/Main.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace JoinCat;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+
+class Main extends PluginBase implements Listener{
+
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents ($this, $this);
+	}
+
+	public function onPlayerJoin(PlayerJoinEvent $event){
+		$tag = ("§6§l[§f§l 서버§6§l ]§f§l");
+		$player = $event->getPlayer();
+		if(! $player->isOP()){
+			$player->sendMessage($tag."안녕하세요 오늘은 광질을 하시는거 어때요??");
+	}
+
+	if ( $player->isOP()){
+		$tag = ("§6§l[§f§l 서버§6§l ]§f§l");
+		$player = $event->getPlayer();
+		$player->sendMessage($tag."안녕!");
+}
+}
+}
