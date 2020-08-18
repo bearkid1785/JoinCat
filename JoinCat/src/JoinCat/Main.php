70f701009083324a485016b32b1ac76d1a5efed8
bearkid1785 <69457316+bearkid1785@users.noreply.github.com>
2020-08-18 09:08:27 +0900

    1.0.1 업데이트
    멘트가 너무 허접해 멘트 수정

--- JoinCat/src/JoinCat/Main.php
+++ JoinCat/src/JoinCat/Main.php
@@ -17,13 +17,13 @@ class Main extends PluginBase implements Listener{
 		$tag = ("§6§l[§f§l 서버§6§l ]§f§l");
 		$player = $event->getPlayer();
 		if(! $player->isOP()){
-			$player->sendMessage($tag."안녕하세요 오늘은 광질을 하시는거 어때요??");
+			$player->sendMessage($tag . "안녕하세요 오늘은 광질을 하시는거 어때요??");
 	}
 
 	if ( $player->isOP()){
 		$tag = ("§6§l[§f§l 서버§6§l ]§f§l");
 		$player = $event->getPlayer();
-		$player->sendMessage($tag."안녕!");
-}
+		$player->sendMessage($tag . "안녕! 오늘은 악성유저를 처벌하는게 어때요?");
+	}
 }
 }
